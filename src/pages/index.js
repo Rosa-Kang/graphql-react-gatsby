0013f822a373d82cda0b8164e7af01d32e0a404a
Rosa Kang <therosessom@gmail.com>
2023-03-12 19:53:32 -0700

    add testimonials

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,15 @@ import * as React from "react"
 import Hero from "../components/Hero"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Testimonials from "../components/Testimonials"
 import Trips from "../components/Trips"
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
     <Hero />
-    <Trips/>
+    <Trips heading="Our Favorite Destinations" />
+    <Testimonials/>
   </Layout>
 )
 
